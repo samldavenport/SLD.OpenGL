d60cc62b3f41eed520401c89f119984886b11c55
thereclaimer <sam.davenport@thereclaimergame.com>
2026-05-22 13:19:52 -0500

    gl context methods

--- include/sld-opengl.hpp
+++ include/sld-opengl.hpp
@@ -5,20 +5,48 @@
 #include <GL/gl.h>
 #include <GL/glext.h>
 
+#ifdef SLD_OPENGL_STATIC
+#   define SLD_OPENGL_API
+#elif defined(SLD_DLL_EXPORT)
+#   define SLD_OPENGL_API __declspec(dllexport)
+#else
+#   define SLD_OPENGL_API __declspec(dllimport)
+#endif
+
 namespace sld {
 
+    //--------------------------------------------------------------------
+    // PRIMITIVE TYPES
+    //--------------------------------------------------------------------
 
+    using gl_value         = GLint;
+    using gl_object        = GLuint;
+    using gl_type          = GLenum
+    using gl_error         = gl_value;
+    using gl_uniform       = gl_value;
+    using gl_status        = gl_value; 
+    using gl_program       = gl_object;
+    using gl_shader        = gl_object;
+    using gl_vertex        = gl_object; 
+    using gl_attribute     = gl_object; 
+    using gl_buffer        = gl_object;
+    using gl_buffer_type   = gl_type;
+    using gl_buffer_useage = gl_type;
 
+    //--------------------------------------------------------------------
+    // STRUCTURED TYPES
+    //--------------------------------------------------------------------
 
-    struct gl_context;
-    struct gl_object;
-    struct gl_val;
-
-
-    struct gl_object {
-
+    struct gl_context {
+        gl_error   error;
+        gl_program program;
+        gl_buffer  buffer;
     };
 
+    SLD_OPENGL_API void gl_context_init                   (gl_context* ctx);
+    SLD_OPENGL_API void gl_context_clear_errors           (gl_context* ctx);
+    SLD_OPENGL_API void gl_context_enable_depth_buffering (gl_context* ctx);
+    SLD_OPENGL_API void gl_context_enable_smoothing       (gl_context* ctx);
 };
 
 #endif //SLD_OPENGL_HPP
