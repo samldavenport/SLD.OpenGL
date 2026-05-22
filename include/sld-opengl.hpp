7f6416d64ce80bb0c6b345bfb3fd92a597f4f085
thereclaimer <sam.davenport@thereclaimergame.com>
2026-05-22 13:03:29 -0500

    added core submodule

--- include/sld-opengl.hpp
+++ include/sld-opengl.hpp
@@ -1,7 +1,23 @@
 #ifndef SLD_OPENGL_HPP
 #define SLD_OPENGL_HPP
 
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glext.h>
+
 namespace sld {
+
+
+
+
+    struct gl_context;
+    struct gl_object;
+    struct gl_val;
+
+
+    struct gl_object {
+
+    };
 
 };
 
